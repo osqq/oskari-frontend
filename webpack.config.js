f85ed214c0170a6fd837f85e148e3a5b69772a4f
Pekka Helesuo <pekka.helesuo@siili.com>
2025-10-07 12:19:44 +0300

    fix issue in dev/prod olcs import

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ module.exports = (env, argv) => {
 
     // entries are configs for each app, plugins accumulate resource copying etc from all the apps
     const { entries, plugins } = generateEntries(appsetupPaths, isProd, __dirname);
-
-    plugins.push(new NormalModuleReplacementPlugin(/^olcs\/css\/olcs\.css$/, path.resolve(__dirname, 'node_modules/olcs/css/olcs.css')))
+    const olcsPath = isProd ? path.resolve(process.cwd(), './node_modules/olcs/css/olcs.css') : path.resolve(process.cwd(), './node_modules/oskari-frontend/node_modules/olcs/css/olcs.css');
+    plugins.push(new NormalModuleReplacementPlugin(/^olcs\/css\/olcs\.css$/, olcsPath));
     plugins.push(new MiniCssExtractPlugin({
         filename: '[name]/oskari.min.css'
     }));
