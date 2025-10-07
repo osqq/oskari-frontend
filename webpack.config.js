a30380033bc203082f341fafbf83009360f36376
Pekka Helesuo <pekka.helesuo@siili.com>
2025-10-07 16:53:41 +0300

    identify symlink to decides whether this is production-like or dev

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,12 @@ module.exports = (env, argv) => {
 
     // entries are configs for each app, plugins accumulate resource copying etc from all the apps
     const { entries, plugins } = generateEntries(appsetupPaths, isProd, __dirname);
-    const olcsPath = isProd ? path.resolve(process.cwd(), './node_modules/olcs/css/olcs.css') : path.resolve(process.cwd(), './node_modules/oskari-frontend/node_modules/olcs/css/olcs.css');
+
+    const devPath = path.resolve(process.cwd(), './node_modules/oskari-frontend/node_modules/olcs/css/olcs.css'); // works with build:dev & start:dev
+    const prodPath = path.resolve(process.cwd(), './node_modules/olcs/css/olcs.css'); // works with build & start
+    const devModeEnabled = lstatSync('./node_modules/oskari-frontend').isSymbolicLink();
+    const olcsPath = devModeEnabled ?  devPath : prodPath;
+
     plugins.push(new NormalModuleReplacementPlugin(/^olcs\/css\/olcs\.css$/, olcsPath));
     plugins.push(new MiniCssExtractPlugin({
         filename: '[name]/oskari.min.css'
